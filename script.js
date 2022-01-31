4bf31ae3af0a4969fe79e248e10ef9398eaed04e
Gregory Brunner <gbrunner@esri.com>
2022-01-30 19:17:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ var geojsonFeature = {
 function onEachFeature(feature, layer) {
     // does this feature have a property named popupContent?
     if (feature.properties && feature.properties["Restaurants "]) {
-        layer.bindPopup(feature.properties.["Restaurants "]);
+        layer.bindPopup(feature.properties["Restaurants "]);
     }
 }
 
